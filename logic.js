bc304bcc933dcfc98192faf4fd0e9bb174015720
franciscojed <54954582+franciscojed@users.noreply.github.com>
2020-01-09 18:32:09 -0500

    My Digital Clock version 2

--- logic.js
+++ logic.js
@@ -1,7 +1,45 @@
-let current_date = new Date();
-let hour = current_date.getHours();
-let minutes = current_date.getMinutes();
-let seconds = current_date.getSeconds();
+function giveMeTheTime() {
 
-this.document.write( "<p>"+ hour + ":" + minutes + "<span>:" + seconds + "</span></p>" );
+    let current_date= new Date();
+    let hour= current_date.getHours();
+    let minutes= current_date.getMinutes();
+    let seconds= current_date.getSeconds();
+    let session;
+    
+    if(hour >= 0 && hour <= 11) {             
+        session = "AM";
+        
+        if(hour == 0) hour = hour + 12;
+        
+        if (hour <= 9) hour = "0"+ hour;
+            
+    }
+    
+    
+    
+    if(hour >= 12 && hour <= 23) {        
+        session = "PM";    
+        
+        if (hour != 12) hour = hour - 12;        
+            
+        if(hour <= 9) hour = "0" + hour;
+            
+    }
+    
+        
+    if(minutes <= 9) minutes = "0" + minutes;
+    
+    if(seconds <= 9) seconds = "0" + seconds;
+    
+
+    let now = "<p>"+ hour + ":" + minutes + "<span>:" + seconds + "</span>" + session + "</p>";
+
+
+    document.getElementById("nowTime").innerHTML= now;
+
+    setTimeout(giveMeTheTime, 1000);
+
+
+}
+
 
